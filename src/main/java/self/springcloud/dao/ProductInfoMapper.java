00b725cdef3048960db92d5c44a53a7a4200fe79
softtiger <revokeyu@hotmail.com>
2020-06-24 21:13:01 +0800

    mybatis generator help project

---
+++ src/main/java/self/springcloud/dao/ProductInfoMapper.java
@@ -0,0 +1,17 @@
+package self.springcloud.dao;
+
+import self.springcloud.domain.ProductInfo;
+
+public interface ProductInfoMapper {
+    int deleteByPrimaryKey(String productId);
+
+    int insert(ProductInfo record);
+
+    int insertSelective(ProductInfo record);
+
+    ProductInfo selectByPrimaryKey(String productId);
+
+    int updateByPrimaryKeySelective(ProductInfo record);
+
+    int updateByPrimaryKey(ProductInfo record);
+}
